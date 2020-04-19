01e749c04747ff7efcb5e93e035358a959fd21e7
wrunion <winterrunion@gmail.com>
2020-04-19 14:18:14 -0700

    Second commit.

---
+++ src/components/NewTicketForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NewTicketForm(props) {
+  return (
+    <React.Fragment>
+      <h3>Form will go here.</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewTicketForm;
